0f6e4b7600f8067761c2c63cf50637edda04ac0f
Norman Ncube <nmncube@gmail.com>
2025-10-27 16:46:06 +0200

    commented out sec module

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ module "vpc" {
   #aws_region   = var.aws_region
 }
 
-module "security_groups" {
-  source       = "./modules/compute"
-  vpc_id       = module.vpc.vpc_id
-  subnet_id    = module.vpc.public_subnet_ids
-}
+# module "security_groups" {
+#   source       = "./modules/compute"
+#   vpc_id       = module.vpc.vpc_id
+#   subnet_id    = module.vpc.public_subnet_ids
+# }
 module "ec2_instances" {
   source       = "./modules/compute"
   subnet_id    = module.vpc.public_subnet_ids
